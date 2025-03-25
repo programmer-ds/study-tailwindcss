3b5112aae419bc880c93d3fc98ed57750362adea
kitchen-truck <k-d-s@naver.com>
2025-03-25 22:47:43 +0900

    Third

---
+++ 01-2) Tailwind Config 파일 외부링크(Font-family)/script/tailwind.config.js
@@ -0,0 +1,15 @@
+//tailwind.config.js
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        // 'sans': ["Single Day", "cursive"],
+        'sans': ['Noto Sans', 'Arial', 'sans-serif'],
+        'single': ["Single Day", "cursive"],
+        'blackhan': ["Black Han Sans", "sans-serif"],
+        'montserrat': ["Montserrat", "sans-serif"],
+        'yclover': ["YClover-Bold"],
+      }
+    }
+  }
+}
